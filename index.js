d7d3595ca86add207dac6c1a54d93f7662946043
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-06-13 21:21:46 +0300

    add

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const delay = ms => {
-    return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(ms);
-    }, ms);
+// const delay = ms => {
+//     return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+// delay(2000).then(logger);
+// delay(1000).then(logger);
+// delay(1500).then(logger);
+
+
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+
+    resolve(updatedUsers);
   });
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = updatedUsers => console.table(updatedUsers);
 
-delay(2000).then(logger);
-delay(1000).then(logger);
-delay(1500).then(logger);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
